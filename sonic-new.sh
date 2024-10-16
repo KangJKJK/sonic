99c3daa57bfa568790671656989f39020660099f
kangjk <96231886+KangJKJK@users.noreply.github.com>
2024-10-16 12:48:48 +0900

    Update sonic-new.sh

--- sonic-new.sh
+++ sonic-new.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 환경 변수 설정
-export WORK="/root/Birds-Sui"
+export WORK="/root/sonic-tx-bot"
 export NVM_DIR="$HOME/.nvm"
 
 # 색상 정의
@@ -11,21 +11,21 @@ YELLOW='\033[1;33m'
 CYAN='\033[0;36m'
 NC='\033[0m' # 색상 초기화
 
-echo -e "${GREEN}Birdsui 봇을 설치합니다.${NC}"
+echo -e "${GREEN}소닉 봇을 설치합니다.${NC}"
 echo -e "${GREEN}스크립트작성자: https://t.me/kjkresearch${NC}"
-echo -e "${GREEN}출처: https://github.com/Bachtran301/Birds-Sui${NC}"
+echo -e "${GREEN}출처: https://github.com/Widiskel/sonic-tx-bot${NC}"
 
 echo -e "${CYAN}이 봇은 다음과 같은 기능을 갖고 있습니다.${NC}"
-echo -e "${CYAN}웜캐칭 / 에그업그레이드/ 에그크래킹 / 각종 태스크${NC}"
+echo -e "${CYAN}오토체크인 / 100트잭 / 마일스톤클레임 / 미스테리박스클레임${NC}"
 
 echo -e "${GREEN}설치 옵션을 선택하세요:${NC}"
-echo -e "${YELLOW}1. Bird 봇 새로 설치${NC}"
+echo -e "${YELLOW}1. 소닉 봇 새로 설치${NC}"
 echo -e "${YELLOW}2. 기존정보 그대로 이용하기(재실행)${NC}"
 read -p "선택: " choice
 
 case $choice in
   1)
-    echo -e "${GREEN}Bird 봇을 새로 설치합니다.${NC}"
+    echo -e "${GREEN}소닉 봇을 새로 설치합니다.${NC}"
 
     # 사전 필수 패키지 설치
     echo -e "${YELLOW}시스템 업데이트 및 필수 패키지 설치 중...${NC}"
@@ -40,7 +40,7 @@ case $choice in
 
     # GitHub에서 코드 복사
     echo -e "${YELLOW}GitHub에서 코드 복사 중...${NC}"
-    git clone https://github.com/Bachtran301/Birds-Sui.git
+    git clone https://github.com/Widiskel/sonic-tx-bot.git "$WORK"
     cd "$WORK"
 
     # Node.js LTS 버전 설치 및 사용
@@ -51,57 +51,27 @@ case $choice in
     nvm use --lts
     npm install
 
-    echo -e "${YELLOW}Web텔레그렘에 접속후 F12를 누르시고 게임을 실행하세요${NC}"
-    read -p "애플리케이션-세션저장소-Birds와 관련된 URL클릭 후 나오는 UserID나 QueryID를 적어두세요 (엔터) : "
-    echo -e "${GREEN}다계정의 query_id를 입력할 경우 줄바꿈으로 구분하세요.${NC}"
-    echo -e "${GREEN}입력을 마치려면 엔터를 두 번 누르세요.${NC}"
-    echo -e "${YELLOW}query_id를 입력하세요:${NC}"
-    
-    # 쿼리 파일 생성 및 초기화
-    {
-        while IFS= read -r line; do
-            [[ -z "$line" ]] && break
-            echo "$line"
-        done
-    } > "$WORK/data.txt"
+    # 개인키 입력받기
+    read -p "Solana의 개인키를 쉼표로 구분하여 입력하세요: " privkeys
+  
+# accounts.js 파일 생성
+cat <<EOL > "$WORK/accounts.js"
+export const account = [
+$(echo "$privkeys" | sed 's/,/\n/g' | sed 's/^/  "/;s/$/",/')
+];
+EOL
 
-    # 사용자에게 프록시 사용 여부를 물어봅니다.
-    read -p "프록시를 사용하시겠습니까? (y/n): " use_proxy
-    
-    if [[ "$use_proxy" == "y" || "$use_proxy" == "Y" ]]; then
-        # 프록시 정보 입력 안내
-        echo -e "${YELLOW}프록시 정보를 입력하세요. 입력형식: http://user:pass@ip:port${NC}"
-        echo -e "${YELLOW}여러 개의 프록시는 줄바꿈으로 구분하세요.${NC}"
-        echo -e "${YELLOW}입력을 마치려면 엔터를 두 번 누르세요.${NC}"
+    echo -e "${GREEN}accounts.js 파일이 성공적으로 생성되었습니다.${NC}"
 
-        # 프록시 정보를 직접 proxy.txt 파일에 저장
-        > "$WORK/proxy.txt"  # 파일 초기화
-        while IFS= read -r line; do
-            [[ -z "$line" ]] && break
-            echo "$line" >> "$WORK/proxy.txt"
-        done
-
-        echo -e "${GREEN}프록시 정보가 proxy.txt 파일에 저장되었습니다.${NC}"
-
-        # 봇 구동
-        node birds.js
-    else
-        node birds-proxy.js
-    fi
+    # 봇 구동
+    node index.js
     ;;
     
   2)
-    echo -e "${GREEN}Bird 봇을 재실행합니다.${NC}"
-    
-    # 사용자에게 프록시 사용 여부를 물어봅니다.
-    read -p "프록시를 사용하시겠습니까? (y/n): " use_proxy
+    echo -e "${GREEN}소닉 봇을 재실행합니다.${NC}"
     cd "$WORK"
     git pull
-    if [[ "$use_proxy" == "y" || "$use_proxy" == "Y" ]]; then
-        node birds.js
-    else
-        node birds-proxy.js
-    fi
+    node index.js
     ;;
 
   *)
