c635011fd182d604b13521c258b6194b21d09e5c
kangjk <96231886+KangJKJK@users.noreply.github.com>
2024-10-18 00:23:48 +0900

    Update sonic-new.sh

--- sonic-new.sh
+++ sonic-new.sh
@@ -57,9 +57,11 @@ case $choice in
     # accounts.js 파일 생성
     cat <<EOL > "$WORK/accounts.js"
     export const account = [
-    $(echo "$privkeys" | sed 's/,/\n/g' | sed 's/^/  "/;s/$/",/')
+    $(echo "$privkeys" | sed 's/,/\n/g' | sed 's/^/  "/;s/$/",/' | sed '$ s/,$//')  # 마지막 쉼표 제거
     ];
     EOL
+    
+    echo -e "${GREEN}accounts.js 파일이 성공적으로 생성되었습니다.${NC}"
 
     echo -e "${GREEN}accounts.js 파일이 성공적으로 생성되었습니다.${NC}"
 
